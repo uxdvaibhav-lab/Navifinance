01eafc8fe4d89f1bb6168ca6421f9b16fcca74af
Vaibhav Agarwal <vaibhav@Vaibhavs-MacBook-Pro.local>
2025-11-10 00:08:34 +0530

    fix: update routing to use HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Showcase from "./pages/Showcase";
@@ -10,14 +10,13 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <CustomCursor />
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/navi-super-app-showcase-34423/"}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/showcase" element={<Showcase />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </QueryClientProvider>
 );
 
