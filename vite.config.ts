c0a8b9e4f4847155409b6e1b09acd5fb4c6d44b5
Vaibhav Agarwal <vaibhav@Vaibhavs-MacBook-Pro.local>
2025-11-09 15:26:50 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/navi-super-app-showcase-34423/',
   server: {
     host: "::",
     port: 8080,
